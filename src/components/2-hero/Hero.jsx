2cda5f8466183bd6643e179cda257d66d2dafc66
abdelbassetali <146743316+abdelbassetali@users.noreply.github.com>
2026-08-23 23:19:45 +0300

    Update Hero.jsx

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -1,51 +1,7 @@
-// import React from "react";
-// import './hero.css';
-// import Lottie from "lottie-react";
-// import { useTypewriter } from 'react-simple-typewriter';
-// function Hero() {
-//    const [typeEffect] = useTypewriter({
-//       words: ['Web GIS Developer'],
-//       loop: true,
-//       typeSpeed: 100,
-//       deleteSpeed: 50,
-//    });
-
-//    return (
-//       <div className="app">
-//          <section className="Hero flex">
-//             <div className="left ">
-//                <div className="img-avatar flex">
-//                   <img className="avatar" src="./react-modified.png" alt="" />
-//                   <div className="icon-verified"></div>
-//                </div>
-//                <button className="btn-left">welcome All In My Portfolio</button>
-//                <h1 className="wwriter">
-//                   Hi, I am Abdel Basset Ali
-//                </h1>
-//                <span className="wwriter">{typeEffect}</span>
-//                <p className="p1">Hello everyone, I am a Web GIS Developer specializing in building interactive web mapping applications and spatial data visualization.</p>
-//                <div className="icon-left flex">
-//                   <div className="icon-1 icon-github"></div>
-//                   <div className="icon-1 icon-twitter"></div>
-//                </div>
-//                <div className="letscon flex">
-//                   <span className="LETS">Let’s Connect</span>
-//                   <span className="icon-arrow-right-circle aroow"></span>
-//                </div>
-//             </div>
-//             <div className="right animation ">
-//               <Lottie path="/anemation/Lottie Lego .json" />
-//          </section>
-//       </div>
-//    );
-// }
-
-// export default Hero;
 import React from "react";
 import './hero.css';
 import Lottie from "lottie-react";
 import { useTypewriter } from 'react-simple-typewriter';
-import legoAnimation from './anemation/Lottie Lego .json';
 
 function Hero() {
    const [typeEffect] = useTypewriter({
@@ -58,7 +14,7 @@ function Hero() {
    return (
       <div className="app">
          <section className="Hero flex">
-            <div className="left ">
+            <div className="left">
                <div className="img-avatar flex">
                   <img className="avatar" src="./react-modified.png" alt="" />
                   <div className="icon-verified"></div>
@@ -78,10 +34,10 @@ function Hero() {
                   <span className="icon-arrow-right-circle aroow"></span>
                </div>
             </div>
-            <div className="right animation ">
-               {/* هنا التعديل: استعملنا الـ animationData اللي استوردناها فوق */}
-               <Lottie animationData={legoAnimation} loop={true} />
-            </div>
+            
+            <div className="right animation">
+                <Lottie path="/anemation/lottie-lego.json" loop={true} />       
+            </div> {/* أضفنا قفلة الـ div هنا */}
          </section>
       </div>
    );
