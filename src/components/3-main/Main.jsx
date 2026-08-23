94efa2bfcadbd1af1a96398818b4d498c0bc6641
abdelbassetali <146743316+abdelbassetali@users.noreply.github.com>
2026-08-23 23:40:52 +0300

    Update Main.jsx

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -31,7 +31,7 @@ const project = [
     },
     { 
         projectTitle: "Modern Furniture Store", 
-        projectDesc: "A sleek and modern furniture store landing page showcasing a stylish chair collection.",
+        projectDesc: "A sleek and modern furniture store landing page showcasing a stylish chair collection, smooth user experience, and responsive design layouts..",
         Category: ["alaa","react"], 
         imgPath: "../4.jpg" 
     },
